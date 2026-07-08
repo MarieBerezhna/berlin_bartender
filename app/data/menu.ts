c2bb36f2da809382de1b8a9e08539bc8aeb64fe5
Marie Berezhna <marie.berezhna@gmail.com>
2026-07-08 22:39:50 +0200

    reorganize specialty coctails

--- app/data/menu.ts
+++ app/data/menu.ts
@@ -247,7 +247,8 @@ const CLASSICS: MenuItem[] = [
     prices:[{label:"cóctel",p:11}]},
 ];
 
-const AUTOR_COCTAILS: MenuItem[] = [
+
+const ACID_TIMES: MenuItem[] = [
   {cat:"Cócteles de autor",family:"Acid Times",name:"Blue Margarita",hasIngr:true,ingr:["Tequila","Licor Bols Blue Curaçao","Sirope de agave","Zumo de lima"],
    prices:[{label:"cóctel",p:12}]},
   {cat:"Cócteles de autor",family:"Acid Times",name:"Brote Primera",hasIngr:true,ingr:["Gin Tanqueray","Licor yuzu","Azúcar líquido","Lima","Albahaca"],
@@ -258,6 +259,9 @@ const AUTOR_COCTAILS: MenuItem[] = [
    prices:[{label:"cóctel",p:12}]},
   {cat:"Cócteles de autor",family:"Acid Times",name:"Berlín Green",hasIngr:true,ingr:["Vodka","Licor Midori","Albahaca","Manzana verde","Maracuya"],
    prices:[{label:"cóctel",p:13}]},
+];
+
+const SWEET_TIMES: MenuItem[] = [
   {cat:"Cócteles de autor",family:"Sweet Times",name:"Porn Star Martini",hasIngr:true,ingr:["Moskovskaya","Licor Bols Vainilla","Maracuya","Zumo de lima","Foam"],
    prices:[{label:"cóctel",p:12}]},
   {cat:"Cócteles de autor",family:"Sweet Times",name:"Éxodo",hasIngr:true,ingr:["Bumbu","Bitter caffè","Sirope pandán","Leche de coco","Café"],
@@ -270,16 +274,31 @@ const AUTOR_COCTAILS: MenuItem[] = [
    prices:[{label:"cóctel",p:12}]},
   {cat:"Cócteles de autor",family:"Sweet Times",name:"Diego",hasIngr:true,ingr:["Jack Daniels Fire","Licor Bols Yogurt","Licor Bols Butterscotch","Sirope amarena","Zumo de arándanos"],
    prices:[{label:"cóctel",p:12}]},
+];
+
+const SPICY_TIMES: MenuItem[] = [
+
   {cat:"Cócteles de autor",family:"Spicy Times",name:"Paloma Don Julio",hasIngr:true,ingr:["Tequila Don Julio","Soda de pomelo","Sirope de agave","Lima"],
    prices:[{label:"cóctel",p:12}]},
   {cat:"Cócteles de autor",family:"Spicy Times",name:"Matarreyes",hasIngr:true,ingr:["Vodka Smirnoff Tamarindo","Licor de humo","Triple Sec Bols","Sirope de agave","Lima","Maracuya"],
    prices:[{label:"cóctel",p:12}]},
+
+];
+
+const HARD_TIMES: MenuItem[] = [
   {cat:"Cócteles de autor",family:"Hard Times",name:"Hombro al sol",hasIngr:true,ingr:["Monkey Shoulder","Fino","Bitter de naranja"],
    prices:[{label:"cóctel",p:12}]},
   {cat:"Cócteles de autor",family:"Hard Times",name:"Polvo de calima",hasIngr:true,ingr:["Diplomático Planas","Ron jamaicano","Chambord","Zumo de naranja","Zumo de piña","Sirope falernum","Zumo de lima"],
    prices:[{label:"cóctel",p:12}]},
   {cat:"Cócteles de autor",family:"Hard Times",name:"Berlin Sling",hasIngr:true,ingr:["Gin Mare","Chambord","Bénédictine DOM","Triple Sec Bols","Piña","Lima"],
    prices:[{label:"cóctel",p:12},{label:"cóctel con chupito de vodka",p:13}]},
+];
+
+const AUTOR_COCTAILS: MenuItem[] = [
+  ...ACID_TIMES,
+  ...SWEET_TIMES,
+  ...SPICY_TIMES,
+  ...HARD_TIMES,
 ];
 
 const LEY_SECA: MenuItem[] = [
